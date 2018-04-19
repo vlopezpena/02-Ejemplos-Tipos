28d4cacb70e79d7c00bf99d0b2ee5e357df84cd8
vlopezpena <37960063+vlopezpena@users.noreply.github.com>
2018-04-19 11:47:23 -0300

    Add files via upload

---
+++ EjemplosTipos.cpp
@@ -0,0 +1,27 @@
+/*
+ Lopez Pena Vanessa Guadalupe
+ AED
+ 19/04/2018
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	double d = 10.696969696966966969696;
+	unsigned int ui = 10;
+	int i=10;
+	string s = "vane";
+	bool b = true;
+	char c = 'V';
+
+	cout <<"Los tipos de datos son:"<<endl;
+	cout <<"Double: " << d <<endl;
+	cout <<"Unsigned int: "<< ui<<endl;
+	cout <<"Int: "<< i <<endl;
+	cout <<"String: "<< s <<endl;
+	cout <<"Bool: "<< b <<endl;
+	cout <<"Char: "<< c <<endl;
+	return 0;
+}
---
+++ README 1.md
@@ -0,0 +1,6 @@
+# AED
+Lopez Pena Vanessa
+K1051
+02-Ejemplos Tipos
+Problema
+Mediante un ejemplo, demostrar la aplicación de tipos de datos
